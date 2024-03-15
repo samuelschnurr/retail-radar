689236a8217781adea4ac62c8e82a5f6065a18c4
Samuel Schnurr <ssn@objektkultur.de>
2024-03-15 20:06:01 +0100

    Create ShoppingService

---
+++ Io.Schnurr.AiShopper.Services/ShoppingService.cs
@@ -0,0 +1,28 @@
+﻿namespace Io.Schnurr.AiShopper.Services;
+
+internal class ShoppingService
+{
+    private void CreateNewThread()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void CreateMessage()
+    {
+        throw new NotImplementedException();
+    }
+    private void CreateRun()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void GetRunStatus()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void GetMessages()
+    {
+        throw new NotImplementedException();
+    }
+}

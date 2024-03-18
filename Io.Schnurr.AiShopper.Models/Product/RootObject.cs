1fb2ce01dd4e4333f61f9161f865aa3eacd4868f
Samuel Schnurr <ssn@objektkultur.de>
2024-03-18 21:44:11 +0100

    Cleanup empty folders

--- Io.Schnurr.AiShopper.Models/Product/RootObject.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace Io.Schnurr.AiShopper.Models.Product;
-
-public class RootObject
-{
-    [JsonPropertyName("search_results")]
-    public List<SearchResult> SearchResults { get; set; }
-}
--- Io.Schnurr.AiShopper.Models/Product/SearchResult.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Io.Schnurr.AiShopper.Models.Product;
-
-public class SearchResult
-{
-    public int Position { get; set; }
-    public string Link { get; set; }
-}

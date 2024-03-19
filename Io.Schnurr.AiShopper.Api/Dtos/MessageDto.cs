209087468e39662f29c9fa283ec18d13fa81d1ca
Samuel Schnurr <ssn@objektkultur.de>
2024-03-19 13:56:23 +0100

    Add ThreadId to Message Dto and Replace UserMessageDto

--- Io.Schnurr.AiShopper.Api/Dtos/MessageDto.cs
+++ Io.Schnurr.AiShopper.Api/Dtos/MessageDto.cs
@@ -3,12 +3,14 @@
 public class MessageDto
 {
     public string Id { get; set; }
+    public string ThreadId { get; set; }
     public string Content { get; set; }
     public string Role { get; set; }
 
-    public MessageDto(string id, string content, string role)
+    public MessageDto(string id, string threadId, string content, string role)
     {
         Id = id;
+        ThreadId = threadId;
         Content = content;
         Role = role;
     }
--- Io.Schnurr.AiShopper.Api/Dtos/UserMessageDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Io.Schnurr.AiShopper.Api.Dtos;
-
-public class UserMessageDto
-{
-    public required string ThreadId { get; set; }
-    public required string Input { get; set; }
-}

0011f17dc5baaed97948d6033d50422f0fbd0cc4
Samuel Schnurr <ssn@objektkultur.de>
2024-04-01 18:04:47 +0200

    Add Routes

--- Io.Schnurr.AiShopper.App/src/index.tsx
+++ Io.Schnurr.AiShopper.App/src/index.tsx
@@ -3,14 +3,20 @@ import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css"
 
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
-import { CustomChat } from "./components/CustomChat"
-import UserJames from "./data/UserJames"
+import { Home } from "./pages/home"
+import { Messenger } from "./pages/messenger"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
 root.render(
     <React.StrictMode>
-        <CustomChat avatar={UserJames} messages={[]} />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="messenger" element={<Messenger />} />
+            </Routes>
+        </BrowserRouter>{" "}
     </React.StrictMode>
 )

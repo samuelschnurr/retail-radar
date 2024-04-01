2642f0584c21fa7bf147f3db90f42e5ce33f281b
Samuel Schnurr <ssn@objektkultur.de>
2024-04-01 19:16:03 +0200

    Add state devtool key and add message method

--- Io.Schnurr.AiShopper.App/src/states/chatConversation.ts
+++ Io.Schnurr.AiShopper.App/src/states/chatConversation.ts
@@ -1,6 +1,7 @@
 import { AvatarProps } from "@chatscope/chat-ui-kit-react/src/components/Avatar/Avatar"
 import { MessageModel } from "@chatscope/chat-ui-kit-react/src/components/Message/Message"
 import { hookstate, useHookstate } from "@hookstate/core"
+import { devtools } from "@hookstate/devtools"
 
 import assistantImage from "./../assets/images/ProfileJames.jpg"
 
@@ -18,6 +19,9 @@ export const defaultState = {
     messages: []
 } as ChatConversation
 
-const state = hookstate<ChatConversation>(defaultState)
+const state = hookstate<ChatConversation>(defaultState, devtools({ key: "chat-conversation" }))
 
 export const useChatConversation = () => useHookstate(state).value
+
+export const addChatConversationMessage = (message: MessageModel) =>
+    state.messages.set(messages => ({ ...messages, message }))

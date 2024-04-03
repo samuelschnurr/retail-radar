e7ac1b8dd9664c708790e3722e1a1e4d1f3e6620
Samuel Schnurr <ssn@objektkultur.de>
2024-04-03 21:40:41 +0200

    Add apiServices

---
+++ Io.Schnurr.AiShopper.App/src/api/threadService.ts
@@ -0,0 +1,12 @@
+import { Thread } from "../types/thread"
+import apiClient from "./apiClient"
+
+export async function postThread(): Promise<Thread> {
+    return apiClient.post("/thread").then(async res => {
+        if (res?.data) {
+            console.log(res.data)
+            return res.data
+        }
+        return null
+    })
+}

f80c6455738cad3522258e2daee0c44c48a967df
Samuel Schnurr <ssn@objektkultur.de>
2024-04-07 15:48:23 +0200

    Implement messageService

--- Io.Schnurr.AiShopper.App/src/api/messageService.ts
+++ Io.Schnurr.AiShopper.App/src/api/messageService.ts
@@ -1,1 +1,20 @@
-export {}
+import { Message } from "../types/message"
+import apiClient from "./apiClient"
+
+export function getMessage(threadId: string, runId: string): Promise<Message> {
+    return apiClient.get(`message?threadId=${threadId}&runId=${runId}`).then(async res => {
+        if (res?.data) {
+            return res.data
+        }
+        return null
+    })
+}
+
+export function postMessage(message: Message): Promise<Message> {
+    return apiClient.post("/message", message).then(async res => {
+        if (res?.data) {
+            return res.data
+        }
+        return null
+    })
+}

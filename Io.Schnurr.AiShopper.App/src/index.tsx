11747a6c062c875fc0e5cad49e0ecaa5becf2057
Samuel Schnurr <ssn@objektkultur.de>
2024-04-20 14:08:12 +0200

    Fix styles

--- Io.Schnurr.AiShopper.App/src/index.tsx
+++ Io.Schnurr.AiShopper.App/src/index.tsx
@@ -1,5 +1,6 @@
 import "./index.css"
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css"
+import "antd/dist/antd.css"
 
 import React from "react"
 import ReactDOM from "react-dom/client"
@@ -7,11 +8,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 import { Home } from "./pages/home"
 import { Messenger } from "./pages/messenger"
+import { Styles } from "./styles/styles"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
 root.render(
     <BrowserRouter>
+        <Styles />
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="messenger" element={<Messenger />} />

6f4d191857997d01c866a22ab47f4257984664d4
Samuel Schnurr <ssn@objektkultur.de>
2024-04-20 13:33:43 +0200

    Add index

--- Io.Schnurr.AiShopper.App/src/pages/home/Home.tsx
+++ Io.Schnurr.AiShopper.App/src/pages/home/Home.tsx
@@ -1,15 +1,35 @@
-import { useNavigate } from "react-router-dom"
+import { lazy } from "react"
+
+import Footer from "../../components/Footer"
+import Header from "../../components/Header"
+import IntroContent from "../../content/IntroContent.json"
+import MiddleBlockContent from "../../content/MiddleBlockContent.json"
+const MiddleBlock = lazy(() => import("../../components/MiddleBlock"))
+const Container = lazy(() => import("../../common/Container"))
+const ScrollToTop = lazy(() => import("../../common/ScrollToTop"))
+const ContentBlock = lazy(() => import("../../components/ContentBlock"))
 
 const Home = () => {
-    const navigate = useNavigate()
-
     return (
-        <button
-            onClick={() => {
-                navigate("/messenger")
-            }}>
-            Start Chat
-        </button>
+        <>
+            <Header />
+            <Container>
+                <ScrollToTop />
+                <ContentBlock
+                    direction="right"
+                    title={IntroContent.title}
+                    content={IntroContent.text}
+                    icon="developer.svg"
+                    id="intro"
+                />
+                <MiddleBlock
+                    title={MiddleBlockContent.title}
+                    content={MiddleBlockContent.text}
+                    button={MiddleBlockContent.button}
+                />
+            </Container>
+            <Footer />
+        </>
     )
 }
 

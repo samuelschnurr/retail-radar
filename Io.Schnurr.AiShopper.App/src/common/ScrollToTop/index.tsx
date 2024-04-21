fa09891f8d8aac0e953a470529f82fb22d16d9ce
Samuel Schnurr <ssn@objektkultur.de>
2024-04-21 18:38:31 +0200

    Update images

--- Io.Schnurr.AiShopper.App/src/common/ScrollToTop/index.tsx
+++ Io.Schnurr.AiShopper.App/src/common/ScrollToTop/index.tsx
@@ -1,42 +1,49 @@
-import { useCallback, useEffect, useState } from "react";
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useCallback, useEffect, useState } from "react"
 
-import { SvgIcon } from "../SvgIcon";
-import { ScrollUpContainer } from "./styles";
+import { SvgIcon } from "../SvgIcon"
+import { ScrollUpContainer } from "./styles"
 
 const ScrollToTop = () => {
-  const [showScroll, setShowScroll] = useState(false);
+    const [showScroll, setShowScroll] = useState(false)
 
-  const checkScrollTop = useCallback(() => {
-    const offsetFromTop = window.scrollY;
+    const checkScrollTop = useCallback(() => {
+        const offsetFromTop = window.scrollY
 
-    if (!showScroll && offsetFromTop > 350) {
-      setShowScroll(true);
-    } else if (offsetFromTop <= 350) {
-      setShowScroll(false);
+        if (!showScroll && offsetFromTop > 350) {
+            setShowScroll(true)
+        } else if (offsetFromTop <= 350) {
+            setShowScroll(false)
+        }
+    }, [showScroll])
+
+    useEffect(() => {
+        window.addEventListener("scroll", checkScrollTop)
+        return () => {
+            window.removeEventListener("scroll", checkScrollTop)
+        }
+    }, [checkScrollTop])
+
+    const scrollUp = () => {
+        const element = document.getElementById("intro") as HTMLDivElement
+        element.scrollIntoView({
+            behavior: "smooth",
+            block: "end",
+            inline: "nearest"
+        })
     }
-  }, [showScroll]);
 
-  useEffect(() => {
-    window.addEventListener("scroll", checkScrollTop);
-    return () => {
-      window.removeEventListener("scroll", checkScrollTop);
-    };
-  }, [checkScrollTop]);
+    return (
+        <ScrollUpContainer onClick={scrollUp} show={showScroll}>
+            <FontAwesomeIcon
+                icon={faChevronUp}
+                flip="horizontal"
+                size="xl"
+                color="rgb(46, 24, 106)"
+            />
+        </ScrollUpContainer>
+    )
+}
 
-  const scrollUp = () => {
-    const element = document.getElementById("intro") as HTMLDivElement;
-    element.scrollIntoView({
-      behavior: "smooth",
-      block: "end",
-      inline: "nearest",
-    });
-  };
-
-  return (
-    <ScrollUpContainer onClick={scrollUp} show={showScroll}>
-      <SvgIcon src="scroll-top.svg" width="20px" height="20px" />
-    </ScrollUpContainer>
-  );
-};
-
-export default ScrollToTop;
+export default ScrollToTop

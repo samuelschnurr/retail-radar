c4ee1e8612469f132828bc77395c4a18ede0a8a6
Samuel Schnurr <ssn@objektkultur.de>
2024-04-28 13:19:02 +0200

    Update globalStyle

--- Io.Schnurr.AiShopper.App/src/index.ts
+++ Io.Schnurr.AiShopper.App/src/index.ts
@@ -7,7 +7,6 @@ export const Styles = createGlobalStyle`
         font-family: Verdana, Geneva, Tahoma, sans-serif;
     }
 
-
     body {
         margin:0;
         padding:0;
@@ -15,26 +14,6 @@ export const Styles = createGlobalStyle`
         outline: 0;
         background: #fff;
         overflow-x: hidden;
-    }
-
-    a:hover {
-        color: #18216d;
-    }
-
-    input,
-    textarea {
-        border-radius: 4px;
-        border: 0;
-        background: rgb(241, 242, 243);
-        transition: all 0.3s ease-in-out;  
-        outline: none;
-        width: 100%;  
-        padding: 1rem 1.25rem;
-
-        :focus-within {
-            background: none;
-            box-shadow: #2e186a 0px 0px 0px 1px;
-        }
     }
 
     h1,
@@ -63,10 +42,6 @@ export const Styles = createGlobalStyle`
         line-height: 1.41;
     }
 
-    h1 {
-        font-weight: 600;
-    }
-
     a {
         text-decoration: none;
         outline: none;
@@ -75,24 +50,5 @@ export const Styles = createGlobalStyle`
         :hover {
             color: #2e186a;
         }
-    }
-    
-    *:focus {
-        outline: none;
-    }
-
-    .about-block-image svg {
-        text-align: center;
-    }
-
-    .ant-drawer-body {
-        display: flex;
-        flex-direction: column;
-        text-align: left;
-        padding-top: 1.5rem;
-    }
-
-    .ant-drawer-content-wrapper {
-        width: 300px !important;
     }
 `

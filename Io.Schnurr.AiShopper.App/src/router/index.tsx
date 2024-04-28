8fec2bbcf99ef40381f7d592c323a6e1a36b88d8
Samuel Schnurr <ssn@objektkultur.de>
2024-04-28 16:12:09 +0200

    Update home page

--- Io.Schnurr.AiShopper.App/src/router/index.tsx
+++ Io.Schnurr.AiShopper.App/src/router/index.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
-import { Home } from "../pages/home"
+import Home from "../pages/home"
 import Messenger from "../pages/messenger"
 
 const Router = () => (

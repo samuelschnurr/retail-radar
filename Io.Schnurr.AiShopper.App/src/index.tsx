182292a2e26396ccd6dfacc91d8c4b39db461196
Samuel Schnurr <ssn@objektkultur.de>
2024-04-28 13:12:08 +0200

    Move globalstyles

--- Io.Schnurr.AiShopper.App/src/index.tsx
+++ Io.Schnurr.AiShopper.App/src/index.tsx
@@ -1,12 +1,13 @@
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css"
 import "antd/dist/antd.css"
 
+import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
+import { Styles } from "./index.ts"
 import { Home } from "./pages/home"
 import { Messenger } from "./pages/messenger"
-import { Styles } from "./styles/styles"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
--- Io.Schnurr.AiShopper.App/src/types/chatPartner.ts
+++
@@ -1,3 +0,0 @@
-import { AvatarProps } from "@chatscope/chat-ui-kit-react/src/components/Avatar/Avatar"
-
-export type ChatPartner = AvatarProps & { info: string }

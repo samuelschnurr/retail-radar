1d0487a321addd792a7eb6cd876f74e398b387ca
Samuel Schnurr <ssn@objektkultur.de>
2024-05-01 21:00:24 +0200

    Make logo href dynamic

--- Io.Schnurr.AiShopper.App/src/components/common/Logo/index.tsx
+++ Io.Schnurr.AiShopper.App/src/components/common/Logo/index.tsx
@@ -1,9 +1,14 @@
+import { LogoContainer } from "./styles"
 import { LogoProps } from "./types"
 
 const Logo = (props: LogoProps) => {
-    const { src, width, height } = props
+    const { src, width, height, href } = props
 
-    return <img src={`/images/logo/${src}`} alt={src} width={width} height={height} />
+    return (
+        <LogoContainer to={href ?? ""}>
+            <img src={`/images/logo/${src}`} alt={src} width={width} height={height} />
+        </LogoContainer>
+    )
 }
 
 export default Logo
--- Io.Schnurr.AiShopper.App/src/components/common/Logo/types.ts
+++ Io.Schnurr.AiShopper.App/src/components/common/Logo/types.ts
@@ -2,4 +2,5 @@ export interface LogoProps {
     src: string
     width: string
     height: string
+    href?: string
 }

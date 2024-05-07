ed0fd203d65eade54f5e9b675f32e27c760b0b1c
Samuel Schnurr <ssn@objektkultur.de>
2024-05-07 18:53:48 +0200

    Refactor endblock

---
+++ Io.Schnurr.RetailRadar.Frontend/src/components/homepage/EndBlock/index.tsx
@@ -0,0 +1,53 @@
+import { Col, Row } from "antd"
+import { Slide } from "react-awesome-reveal"
+
+import Button from "../../common/Button"
+import Content from "../../common/Content"
+import Image from "../../common/Image"
+import { EndBlockSection, StyledImageContainer } from "./styles"
+import { EndBlockProps } from "./types"
+
+const EndBlock = (props: EndBlockProps) => {
+    const { title, subTitle, subTitle2, src } = props
+    return (
+        <EndBlockSection>
+            <Slide direction="right" triggerOnce>
+                <h6>{title}</h6>
+                <Row justify="center" align="middle">
+                    <Col lg={14} md={16} xs={24}>
+                        <Content content={subTitle} />
+                        <Content content={subTitle2} />
+                        <Content
+                            content={
+                                <Button
+                                    onClick={() => {
+                                        window.open("https://ko-fi.com/sampa", "_blank")
+                                    }}>
+                                    <StyledImageContainer>
+                                        <Image height="40px" width="60px" src="/button/kofi.png" />
+                                        Mit einem Kaffee unterstützen
+                                    </StyledImageContainer>
+                                </Button>
+                            }
+                        />
+                    </Col>
+                    <Col lg={10} md={8} xs={24}>
+                        <Content
+                            content={
+                                <Image
+                                    src={src}
+                                    width="100%"
+                                    height="100%"
+                                    shrinkOnMobile={true}
+                                    applyStyling={true}
+                                />
+                            }
+                        />
+                    </Col>
+                </Row>
+            </Slide>
+        </EndBlockSection>
+    )
+}
+
+export default EndBlock

894598fe41733fde89617ac92642c5452aa50579
Samuel Schnurr <ssn@objektkultur.de>
2024-05-08 14:06:18 +0200

    Add imprint margin

--- Io.Schnurr.RetailRadar.Frontend/src/pages/imprint/index.tsx
+++ Io.Schnurr.RetailRadar.Frontend/src/pages/imprint/index.tsx
@@ -4,14 +4,17 @@ import Footer from "../../components/homepage/Footer"
 import Header from "../../components/homepage/Header"
 import HeaderContent from "../../content/HeaderContent.json"
 import ImprintContent from "../../content/ImprintContent.json"
+import { ImprintSection } from "./styles"
 
 const Imprint = () => {
     return (
         <>
             <Header src={HeaderContent.src} button={HeaderContent.button} />
-            <Container>
-                <CommonBlock content={ImprintContent} />
-            </Container>
+            <ImprintSection>
+                <Container>
+                    <CommonBlock content={ImprintContent} />
+                </Container>
+            </ImprintSection>
             <Footer />
         </>
     )

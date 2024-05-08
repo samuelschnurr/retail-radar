70257a9dc5dffe648e03561c86ab1f34802350ae
Samuel Schnurr <ssn@objektkultur.de>
2024-05-08 13:21:42 +0200

    Add routes

--- Io.Schnurr.RetailRadar.Frontend/src/router/index.tsx
+++ Io.Schnurr.RetailRadar.Frontend/src/router/index.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 import Homepage from "../pages/homepage"
+import Imprint from "../pages/imprint"
 import Messenger from "../pages/messenger"
+import Privacy from "../pages/privacy"
 
 const Router = () => (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<Homepage />} />
             <Route path="messenger" element={<Messenger />} />
+            <Route path="impressum" element={<Imprint />} />
+            <Route path="datenschutz" element={<Privacy />} />
         </Routes>
     </BrowserRouter>
 )

f8ff6d21508ebdf4ececa7793ea254bda59444fd
Samuel Schnurr <ssn@objektkultur.de>
2024-05-08 12:31:02 +0200

    Remove lazy component import

--- Io.Schnurr.RetailRadar.Frontend/src/pages/homepage/index.tsx
+++ Io.Schnurr.RetailRadar.Frontend/src/pages/homepage/index.tsx
@@ -1,16 +1,14 @@
-import { lazy } from "react"
-
+import Container from "../../components/common/Container"
+import ScrollToTop from "../../components/common/ScrollToTop"
+import EndBlock from "../../components/homepage/EndBlock"
 import Footer from "../../components/homepage/Footer"
 import Header from "../../components/homepage/Header"
+import IntroBlock from "../../components/homepage/IntroBlock"
+import MiddleBlock from "../../components/homepage/MiddleBlock"
 import EndBlockContent from "../../content/EndBlockContent.json"
 import HeaderContent from "../../content/HeaderContent.json"
 import IntroBlockContent from "../../content/IntroBlockContent.json"
 import MiddleBlockContent from "../../content/MiddleBlockContent.json"
-const MiddleBlock = lazy(() => import("../../components/homepage/MiddleBlock"))
-const Container = lazy(() => import("../../components/common/Container"))
-const ScrollToTop = lazy(() => import("../../components/common/ScrollToTop"))
-const IntroBlock = lazy(() => import("../../components/homepage/IntroBlock"))
-const EndBlock = lazy(() => import("../../components/homepage/EndBlock"))
 
 const Homepage = () => {
     return (

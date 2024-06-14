bbb928ff697ae5b42914db92c5b521e937c597de
Samuel Schnurr <ssn@objektkultur.de>
2024-06-14 16:37:10 +0200

    Add ToolbarContent Title Buttons

--- Io.Schnurr.RetailRadar.Frontend/src/components/messenger/ConversationHeaderContainer/index.tsx
+++ Io.Schnurr.RetailRadar.Frontend/src/components/messenger/ConversationHeaderContainer/index.tsx
@@ -7,6 +7,7 @@ import { resetConversation } from "../../../states/conversation"
 import { resetThread, useThread } from "../../../states/thread"
 import AvatarIcon from "../../common/AvatarIcon"
 import BounceButton from "../../common/BounceButton"
+import ToolbarContent from "./../../../content/ToolbarContent.json"
 import { StyledButton, StyledConversationHeader } from "./styles"
 import { ConversationHeaderContainerProps } from "./types"
 
@@ -28,6 +29,7 @@ const ConversationHeaderContainer = (_props: ConversationHeaderContainerProps) =
                     startDelay={30000}
                     intervalDelay={10000}
                     bounceDuration={5000}
+                    title={ToolbarContent.supportButton.title}
                     onClick={() => {
                         window.open("https://ko-fi.com/sampa", "_blank")
                     }}
@@ -35,6 +37,7 @@ const ConversationHeaderContainer = (_props: ConversationHeaderContainerProps) =
                 <StyledButton
                     icon={<SyncOutlined />}
                     disabled={isLoading}
+                    title={ToolbarContent.refreshButton.title}
                     onClick={() => {
                         resetThread()
                         resetConversation()
@@ -42,6 +45,7 @@ const ConversationHeaderContainer = (_props: ConversationHeaderContainerProps) =
                 />
                 <StyledButton
                     icon={<CloseOutlined />}
+                    title={ToolbarContent.closeButton.title}
                     onClick={() => {
                         navigate("/")
                     }}

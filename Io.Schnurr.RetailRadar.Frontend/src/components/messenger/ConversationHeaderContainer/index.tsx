db0f0be92a2b7b60ee9d5730b20513f5ad6d18fa
Samuel Schnurr <ssn@objektkultur.de>
2024-06-14 17:02:21 +0200

    Add bug report mail to

--- Io.Schnurr.RetailRadar.Frontend/src/components/messenger/ConversationHeaderContainer/index.tsx
+++ Io.Schnurr.RetailRadar.Frontend/src/components/messenger/ConversationHeaderContainer/index.tsx
@@ -1,9 +1,10 @@
-import { CloseOutlined, CoffeeOutlined, SyncOutlined } from "@ant-design/icons"
+import { BugOutlined, CloseOutlined, CoffeeOutlined, SyncOutlined } from "@ant-design/icons"
 import { Avatar, ConversationHeader, UserStatus } from "@chatscope/chat-ui-kit-react"
+import { Divider } from "antd"
 import { useNavigate } from "react-router-dom"
 
 import avatar from "../../../content/AvatarContent.json"
-import { resetConversation } from "../../../states/conversation"
+import { resetConversation, useConversation } from "../../../states/conversation"
 import { resetThread, useThread } from "../../../states/thread"
 import AvatarIcon from "../../common/AvatarIcon"
 import BounceButton from "../../common/BounceButton"
@@ -13,7 +14,22 @@ import { ConversationHeaderContainerProps } from "./types"
 
 const ConversationHeaderContainer = (_props: ConversationHeaderContainerProps) => {
     const { isLoading } = useThread()
+    const { messages } = useConversation()
     const navigate = useNavigate()
+
+    const openDonationSite = () => {
+        window.open("https://ko-fi.com/sampa", "_blank")
+    }
+
+    const sendBugReport = () => {
+        const mailReceiver = ToolbarContent.bugButton.mailReceiver
+        const subject = encodeURIComponent(ToolbarContent.bugButton.mailTitle)
+        const serializedJson = JSON.stringify(messages, null, 2)
+        const body = encodeURIComponent(ToolbarContent.bugButton.mailBody + serializedJson)
+        const mailtoLink = `mailto:${mailReceiver}?subject=${subject}&body=${body}`
+        window.location.href = mailtoLink
+    }
+
     return (
         <StyledConversationHeader>
             <AvatarIcon
@@ -29,11 +45,15 @@ const ConversationHeaderContainer = (_props: ConversationHeaderContainerProps) =
                     startDelay={30000}
                     intervalDelay={10000}
                     bounceDuration={5000}
-                    title={ToolbarContent.supportButton.title}
-                    onClick={() => {
-                        window.open("https://ko-fi.com/sampa", "_blank")
-                    }}
+                    title={ToolbarContent.donationButton.title}
+                    onClick={openDonationSite}
                 />
+                <StyledButton
+                    icon={<BugOutlined />}
+                    title={ToolbarContent.bugButton.title}
+                    onClick={sendBugReport}
+                />
+                <Divider type="vertical" />
                 <StyledButton
                     icon={<SyncOutlined />}
                     disabled={isLoading}

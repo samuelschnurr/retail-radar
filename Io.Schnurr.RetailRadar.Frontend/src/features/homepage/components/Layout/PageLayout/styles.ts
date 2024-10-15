d521476bb533995655456a7aa6a8eb25308f3ba3
Samuel Schnurr <ssn@objektkultur.de>
2024-10-15 18:06:02 +0200

    Refactor padding in PageLayout component

--- Io.Schnurr.RetailRadar.Frontend/src/features/homepage/components/Layout/PageLayout/styles.ts
+++ Io.Schnurr.RetailRadar.Frontend/src/features/homepage/components/Layout/PageLayout/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components"
 
 export const Section = styled.section`
     position: relative;
-    padding: 0rem 0 32rem;
+    padding: 0 0 32rem;
 
     @media only screen and (max-width: ${p => p.theme.breakpoints.large}) {
         padding: 4rem 0 16rem;

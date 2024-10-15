ca2726a40eaaa017d9d96084449542713f10921e
Samuel Schnurr <ssn@objektkultur.de>
2024-10-15 18:26:48 +0200

    Refactor homepage component to use new naming conventions and remove unused imports

--- Io.Schnurr.RetailRadar.Frontend/src/router/index.tsx
+++ Io.Schnurr.RetailRadar.Frontend/src/router/index.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
-import Homepage from "../pages/Homepage"
+import Home from "../pages/Home"
 import Imprint from "../pages/Imprint"
 import Messenger from "../pages/Messenger"
 import NotFound from "../pages/NotFound"
@@ -9,7 +9,7 @@ import Privacy from "../pages/Privacy"
 const Router = () => (
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Homepage />} />
+            <Route path="/" element={<Home />} />
             <Route path="/messenger" element={<Messenger />} />
             <Route path="/impressum" element={<Imprint />} />
             <Route path="/datenschutz" element={<Privacy />} />

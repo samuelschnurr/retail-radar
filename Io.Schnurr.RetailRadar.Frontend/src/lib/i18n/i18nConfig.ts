ccc9c10870a3547490dbf83d95b021b879ca3226
Samuel Schnurr <ssn@objektkultur.de>
2024-11-27 18:16:21 +0100

    add i18nConfig

--- Io.Schnurr.RetailRadar.Frontend/src/lib/i18n/i18nConfig.ts
+++ Io.Schnurr.RetailRadar.Frontend/src/lib/i18n/i18nConfig.ts
@@ -1,15 +1,16 @@
 import i18n from "i18next"
+import Backend from "i18next-http-backend"
 import { initReactI18next } from "react-i18next"
 
-import { resources } from "./i18nResources"
+const i18nConfig = i18n
+    .use(Backend)
+    .use(initReactI18next)
+    .init({
+        lng: "en",
+        fallbackLng: "en",
+        interpolation: {
+            escapeValue: false
+        }
+    })
 
-i18n.use(initReactI18next).init({
-    resources,
-    lng: "en",
-    fallbackLng: "en",
-    interpolation: {
-        escapeValue: false
-    }
-})
-
-export default i18n
+export default i18nConfig

a6cce53eca9d1242069b4a594a081f930098727f
Samuel Schnurr <ssn@objektkultur.de>
2024-11-27 19:24:37 +0100

    Refactor Imprint component to use utility function for localized resource retrieval

--- Io.Schnurr.RetailRadar.Frontend/src/pages/Imprint.tsx
+++ Io.Schnurr.RetailRadar.Frontend/src/pages/Imprint.tsx
@@ -1,11 +1,11 @@
 import CommonBlock from "@features/landingpage/components/Common/CommonSection"
 import PageLayout from "@features/landingpage/components/Layout/PageLayout"
-import ImprintContent from "@features/landingpage/locales/de/ImprintContent.json"
+import { getLocalizedResourceFile } from "@lib/i18n/i18nUtils"
 
 const Imprint = () => {
     return (
         <PageLayout>
-            <CommonBlock content={ImprintContent} />
+            <CommonBlock content={getLocalizedResourceFile("ImprintContent")} />
         </PageLayout>
     )
 }

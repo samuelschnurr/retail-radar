df26725e893d5ea63c78811540b9f9c4aa5c9027
Samuel Schnurr <ssn@objektkultur.de>
2024-11-28 17:28:47 +0100

    Refactor Header component to use localized resource and update props for consistency

--- Io.Schnurr.RetailRadar.Frontend/src/features/landingpage/components/Layout/PageLayout/index.tsx
+++ Io.Schnurr.RetailRadar.Frontend/src/features/landingpage/components/Layout/PageLayout/index.tsx
@@ -1,7 +1,7 @@
 import Footer from "@features/landingpage/components/Layout/Footer"
 import Header from "@features/landingpage/components/Layout/Header"
 import FooterContent from "@features/landingpage/locales/de/FooterContent.json"
-import HeaderContent from "@features/landingpage/locales/de/HeaderContent.json"
+import { useTranslation } from "react-i18next"
 
 import CenteredContainer from "../CenteredContainer"
 import { Section } from "./styles"
@@ -9,10 +9,15 @@ import { PageLayoutProps } from "./types"
 
 const PageLayout = (props: PageLayoutProps) => {
     const { children } = props
+    const { t } = useTranslation("Header")
 
     return (
         <>
-            <Header src={HeaderContent.src} button={HeaderContent.button} />
+            <Header
+                logoSource={t("logoSource")}
+                buttonText={t("buttonText")}
+                buttonLink={t("buttonLink")}
+            />
             <Section>
                 <CenteredContainer>{children}</CenteredContainer>
             </Section>

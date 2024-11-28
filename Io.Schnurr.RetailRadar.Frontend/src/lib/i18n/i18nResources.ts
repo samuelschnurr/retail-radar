07e3589275fae0b89e92fd0ce8e168ad39aa5e97
Samuel Schnurr <ssn@objektkultur.de>
2024-11-28 17:18:40 +0100

    Add Privacy localized resource and update Privacy component to use utility function

--- Io.Schnurr.RetailRadar.Frontend/src/lib/i18n/i18nResources.ts
+++ Io.Schnurr.RetailRadar.Frontend/src/lib/i18n/i18nResources.ts
@@ -1,10 +1,14 @@
 import ImprintDE from "@features/landingpage/locales/de/Imprint.json"
+import PrivacyDE from "@features/landingpage/locales/de/Privacy.json"
 import ImprintEN from "@features/landingpage/locales/en/Imprint.json"
+import PrivacyEN from "@features/landingpage/locales/en/Privacy.json"
 
 export const i18nResourceEN = {
-    Imprint: ImprintEN
+    Imprint: ImprintEN,
+    Privacy: PrivacyEN
 }
 
 export const i18nResourceDE = {
-    Imprint: ImprintDE
+    Imprint: ImprintDE,
+    Privacy: PrivacyDE
 }

67341e05f9e6e2e8455752dc2932efdcf2820aeb
Samuel Schnurr <ssn@objektkultur.de>
2024-11-28 18:10:14 +0100

    Refactor router paths to use '/imprint' and '/privacy' for consistency

--- Io.Schnurr.RetailRadar.Frontend/src/router/index.tsx
+++ Io.Schnurr.RetailRadar.Frontend/src/router/index.tsx
@@ -12,7 +12,8 @@ const Router = () => (
             <Route path="/" element={<Home />} />
             <Route path="/messenger" element={<Messenger />} />
             <Route path="/impressum" element={<Imprint />} />
-            <Route path="/datenschutz" element={<Privacy />} />
+            <Route path="/imprint" element={<Imprint />} />
+            <Route path="/privacy" element={<Privacy />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
     </BrowserRouter>

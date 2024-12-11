c3c3e6c88b4b6cbf299a1d4a6b248d59d93cbf65
Samuel Schnurr <ssn@objektkultur.de>
2024-12-11 17:38:27 +0100

    Add localization support for Messenger component in English and German

--- Io.Schnurr.RetailRadar.Frontend/src/lib/i18n/i18nConfig.ts
+++ Io.Schnurr.RetailRadar.Frontend/src/lib/i18n/i18nConfig.ts
@@ -1,12 +1,15 @@
 import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
 
-import { landingpageResources } from "./i18nResources"
+import { landingpageResources, messengerResources } from "./i18nResources"
 
 const i18nConfig = i18n.use(initReactI18next).init({
     lng: "en",
     fallbackLng: "en",
-    resources: { en: { ...landingpageResources.en }, de: { ...landingpageResources.de } },
+    resources: {
+        en: { ...landingpageResources.en, ...messengerResources.en },
+        de: { ...landingpageResources.de, ...messengerResources.de }
+    },
     defaultNS: undefined,
     interpolation: {
         escapeValue: false

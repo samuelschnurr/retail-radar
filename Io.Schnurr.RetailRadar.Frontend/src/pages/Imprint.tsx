dd43da362b6e18e4f984751f3b71952b94a5838f
Samuel Schnurr <ssn@objektkultur.de>
2024-12-11 19:11:19 +0100

    Refactor Imprint, NotFound, and Privacy pages to use CommonSection component for consistency

--- Io.Schnurr.RetailRadar.Frontend/src/pages/Imprint.tsx
+++ Io.Schnurr.RetailRadar.Frontend/src/pages/Imprint.tsx
@@ -1,4 +1,4 @@
-import CommonBlock from "@features/landingpage/components/Common/CommonSection"
+import CommonSection from "@features/landingpage/components/Common/CommonSection"
 import PageLayout from "@features/landingpage/components/Layout/PageLayout"
 import { getLocalizedResourceFile } from "@lib/i18n/i18nUtils"
 
@@ -7,7 +7,7 @@ const Imprint = () => {
         <PageLayout
             headerContent={getLocalizedResourceFile("header")}
             footerContent={getLocalizedResourceFile("footer")}>
-            <CommonBlock content={getLocalizedResourceFile("imprint")} />
+            <CommonSection content={getLocalizedResourceFile("imprint")} />
         </PageLayout>
     )
 }

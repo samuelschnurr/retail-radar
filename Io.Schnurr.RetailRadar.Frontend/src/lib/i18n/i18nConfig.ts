170350013748ac3642b3714aaa41df9937b18c8f
Samuel Schnurr <ssn@objektkultur.de>
2024-12-11 18:39:32 +0100

    Add route localization support for English and German in Router component

--- Io.Schnurr.RetailRadar.Frontend/src/lib/i18n/i18nConfig.ts
+++ Io.Schnurr.RetailRadar.Frontend/src/lib/i18n/i18nConfig.ts
@@ -1,14 +1,14 @@
 import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
 
-import { landingpageResources, messengerResources } from "./i18nResources"
+import { landingpageResources, messengerResources, routeResources } from "./i18nResources"
 
 const i18nConfig = i18n.use(initReactI18next).init({
     lng: "en",
     fallbackLng: "en",
     resources: {
-        en: { ...landingpageResources.en, ...messengerResources.en },
-        de: { ...landingpageResources.de, ...messengerResources.de }
+        en: { ...routeResources.en, ...landingpageResources.en, ...messengerResources.en },
+        de: { ...routeResources.de, ...landingpageResources.de, ...messengerResources.de }
     },
     defaultNS: undefined,
     interpolation: {

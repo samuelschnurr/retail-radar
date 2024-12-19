4dd43e38730e7cf4cd3c0dc3eb4937963b7a2c64
Samuel Schnurr <ssn@objektkultur.de>
2024-12-19 18:54:18 +0100

    Enhance i18n configuration by integrating initReactI18next and enabling debug mode for improved language detection

--- Io.Schnurr.RetailRadar.Frontend/src/lib/i18n/i18nConfig.ts
+++ Io.Schnurr.RetailRadar.Frontend/src/lib/i18n/i18nConfig.ts
@@ -1,5 +1,6 @@
 import i18n from "i18next"
 import LanguageDetector from "i18next-browser-languagedetector"
+import { initReactI18next } from "react-i18next"
 
 import { landingpageResources, messengerResources } from "./i18nResources"
 
@@ -14,21 +15,25 @@ const trimmedNavigatorLanguageDetector = {
 const languageDetector = new LanguageDetector()
 languageDetector.addDetector(trimmedNavigatorLanguageDetector)
 
-const i18nConfig = i18n.use(languageDetector).init({
-    load: "languageOnly",
-    fallbackLng: "en",
-    resources: {
-        en: { ...landingpageResources.en, ...messengerResources.en },
-        de: { ...landingpageResources.de, ...messengerResources.de }
-    },
-    defaultNS: undefined,
-    interpolation: {
-        escapeValue: false
-    },
-    detection: {
-        order: ["localStorage", "trimmedNavigator"],
-        caches: ["localStorage"]
-    }
-})
+const i18nConfig = i18n
+    .use(languageDetector)
+    .use(initReactI18next)
+    .init({
+        load: "languageOnly",
+        debug: true,
+        fallbackLng: "en",
+        resources: {
+            en: { ...landingpageResources.en, ...messengerResources.en },
+            de: { ...landingpageResources.de, ...messengerResources.de }
+        },
+        defaultNS: undefined,
+        interpolation: {
+            escapeValue: false
+        },
+        detection: {
+            order: ["localStorage", "trimmedNavigator"],
+            caches: ["localStorage"]
+        }
+    })
 
 export default i18nConfig

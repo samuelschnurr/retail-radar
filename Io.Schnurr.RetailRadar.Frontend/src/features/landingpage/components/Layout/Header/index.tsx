b36a35152e28ccaab1f79bdee1f0026102136646
Samuel Schnurr <ssn@objektkultur.de>
2024-12-19 14:09:50 +0100

    Refactor Header component to use Hookstate for language management and improve language change handling

--- Io.Schnurr.RetailRadar.Frontend/src/features/landingpage/components/Layout/Header/index.tsx
+++ Io.Schnurr.RetailRadar.Frontend/src/features/landingpage/components/Layout/Header/index.tsx
@@ -1,6 +1,7 @@
 import Button from "@features/landingpage/components/Common/Button"
 import Image from "@features/landingpage/components/Common/Image"
 import CenteredContainer from "@features/landingpage/components/Layout/CenteredContainer"
+import { useHookstate } from "@hookstate/core"
 import i18n from "i18next"
 import { useNavigate } from "react-router-dom"
 
@@ -8,16 +9,17 @@ import TextButton from "../../Common/TextButton"
 import { HeaderSection, StyledRow, StyledTextButtonContainer } from "./styles"
 import { HeaderProps } from "./types"
 
-const Header = (props: HeaderProps) => {
-    const { imageSource, buttonUrl, buttonLabel } = props
+const Header = ({ imageSource, buttonUrl, buttonLabel }: HeaderProps) => {
     const navigate = useNavigate()
+    const i18nLanguage = useHookstate(i18n.language)
 
     const changeLanguage = (language: string) => {
         i18n.changeLanguage(language)
+        i18nLanguage.set(language)
     }
 
     const isLanguageActive = (language: string) => {
-        return i18n.language === language
+        return i18nLanguage.get() === language
     }
 
     return (

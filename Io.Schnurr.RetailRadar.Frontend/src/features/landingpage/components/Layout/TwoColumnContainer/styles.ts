0d5c2c0888cfa6fe716fad329d38a90516b670ec
Samuel Schnurr <ssn@objektkultur.de>
2024-12-19 18:01:02 +0100

    Refactor TwoColumnContainer component by moving styled Row to a separate styles file and cleaning up unused imports

---
+++ Io.Schnurr.RetailRadar.Frontend/src/features/landingpage/components/Layout/TwoColumnContainer/styles.ts
@@ -0,0 +1,6 @@
+import { Row } from "antd"
+import styled from "styled-components"
+
+export const StyledRow = styled(Row)<{ flexDirection: string }>`
+    flex-direction: ${p => p.flexDirection};
+`

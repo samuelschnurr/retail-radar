ea26e1cdaf29df9bf7d58b17d3e849aa333e835c
Samuel Schnurr <ssn@objektkultur.de>
2024-12-19 17:10:48 +0100

    Implement language change handling in App component to re-render on language switch

--- Io.Schnurr.RetailRadar.Frontend/src/index.tsx
+++ Io.Schnurr.RetailRadar.Frontend/src/index.tsx
@@ -2,17 +2,34 @@ import "antd/dist/antd.css"
 // Import this way to not be treeshaked
 import "@lib/i18n/i18nConfig"
 
+import i18next from "i18next"
 import ReactDOM from "react-dom/client"
 import { ThemeProvider } from "styled-components"
 
 import Router from "./router"
 import GlobalStyles from "./styles/globalStyles"
 import LightTheme from "./styles/lightTheme"
+
+const App = () => {
+    const renderRoot = () => {
+        root.render(
+            <ThemeProvider theme={LightTheme}>
+                <GlobalStyles />
+                <Router />
+            </ThemeProvider>
+        )
+    }
+
+    i18next.on("languageChanged", renderRoot)
+
+    return (
+        <ThemeProvider theme={LightTheme}>
+            <GlobalStyles />
+            <Router />
+        </ThemeProvider>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
-root.render(
-    <ThemeProvider theme={LightTheme}>
-        <GlobalStyles />
-        <Router />
-    </ThemeProvider>
-)
+root.render(<App />)

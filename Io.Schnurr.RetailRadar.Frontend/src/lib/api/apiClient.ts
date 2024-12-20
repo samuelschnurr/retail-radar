85987e5cc2431127a62994cc147af8d95dca3f1e
Samuel Schnurr <ssn@objektkultur.de>
2024-12-20 10:18:41 +0100

    Add language header to API requests for improved localization

--- Io.Schnurr.RetailRadar.Frontend/src/lib/api/apiClient.ts
+++ Io.Schnurr.RetailRadar.Frontend/src/lib/api/apiClient.ts
@@ -1,10 +1,23 @@
 import { addErrorConversationMessage } from "@features/messenger/states/conversation"
 import axios, { AxiosInstance } from "axios"
+import i18next from "i18next"
 import i18n from "i18next"
 
 const apiClient: AxiosInstance = axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL
 })
+
+apiClient.interceptors.request.use(
+    config => {
+        const currentLanguage = i18next.language
+        config.headers["Accept-Language"] = currentLanguage
+        return config
+    },
+    error => {
+        console.log(error)
+        return Promise.reject(error)
+    }
+)
 
 apiClient.interceptors.response.use(
     response => response,

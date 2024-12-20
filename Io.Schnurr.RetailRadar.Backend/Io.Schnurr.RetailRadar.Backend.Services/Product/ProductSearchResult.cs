e5c82c90e8adadecb05d52b9a817e641793b333c
Samuel Schnurr <ssn@objektkultur.de>
2024-12-20 17:49:56 +0100

    Add ProductSearchResult class with validation method
    Introduce ProductSearchResult class in Io.Schnurr.RetailRadar.Backend.Services.Product namespace. The class includes four internal nullable string properties: SearchTerm, Asin, BaseAddress, and AffiliateId. Added IsValid method to check if all properties are non-null and not whitespace.

---
+++ Io.Schnurr.RetailRadar.Backend/Io.Schnurr.RetailRadar.Backend.Services/Product/ProductSearchResult.cs
@@ -0,0 +1,18 @@
+﻿namespace Io.Schnurr.RetailRadar.Backend.Services.Product;
+
+internal class ProductSearchResult
+{
+    internal string? SearchTerm { get; set; }
+    internal string? Asin { get; set; }
+    internal string? BaseAddress { get; set; }
+    internal string? AffiliateId { get; set; }
+
+    internal bool IsValid()
+    {
+        return
+            !string.IsNullOrWhiteSpace(SearchTerm)
+            && !string.IsNullOrWhiteSpace(Asin)
+            && !string.IsNullOrWhiteSpace(BaseAddress)
+            && !string.IsNullOrWhiteSpace(AffiliateId);
+    }
+}

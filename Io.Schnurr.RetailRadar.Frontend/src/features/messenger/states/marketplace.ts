0735f348a38286c151127a57eb5454ecb3597b03
Samuel Schnurr <ssn@objektkultur.de>
2024-12-21 18:42:13 +0100

    Add first working localstorage state WIP

--- Io.Schnurr.RetailRadar.Frontend/src/features/messenger/states/marketplace.ts
+++ Io.Schnurr.RetailRadar.Frontend/src/features/messenger/states/marketplace.ts
@@ -1,18 +1,19 @@
 import { hookstate, useHookstate } from "@hookstate/core"
 import { devtools } from "@hookstate/devtools"
+import { localstored } from "@hookstate/localstored"
 import i18n from "i18next"
 
 import { Marketplace } from "../types/marketplace"
 import { MarketplaceRegion } from "../types/marketplaceRegion"
 
+const LOCAL_STORAGE_KEY = "retailradar-marketplace"
 const defaultState = { region: i18n.language === "de" ? ".de" : ".com" } as Marketplace
 
-const state = hookstate<Marketplace>(
-    { ...defaultState },
-    devtools({ key: "retailradar-marketplace" })
-)
-
+const state = hookstate<Marketplace>({ ...defaultState }, devtools({ key: LOCAL_STORAGE_KEY }))
 export const useMarketplace = () => useHookstate(state).value
+
+export const useMarketplace2 = () =>
+    useHookstate({ ...defaultState }, localstored({ key: LOCAL_STORAGE_KEY }))
 
 export const resetMarketplace = () => {
     state.set({ ...defaultState })

0e47b5f32c448c4bb564d4f963c8c88baf226527
Samuel Schnurr <ssn@objektkultur.de>
2024-12-22 17:14:17 +0100

    Add message image style

--- Io.Schnurr.RetailRadar.Frontend/src/features/messenger/components/Layout/MessengerLayout/styles.ts
+++ Io.Schnurr.RetailRadar.Frontend/src/features/messenger/components/Layout/MessengerLayout/styles.ts
@@ -7,8 +7,9 @@ export const StyledChatContainer = styled(ChatContainer)`
     .cs-message__content {
         font-family: ${p => p.theme.font.family};
     }
+
     a {
-        color: #1890ff;
+        color: ${p => p.theme.colors.primaryLight};
         text-decoration: none;
         outline: none;
 
@@ -25,6 +26,42 @@ export const StyledChatContainer = styled(ChatContainer)`
             vertical-align: middle;
             margin-left: 0.25rem;
             content: url("/images/icon/link.svg");
+        }
+
+        &.amazonImageLink {
+            background-color: ${p => p.theme.colors.background};
+            display: flex;
+            border-radius: ${p => p.theme.border.radius};
+            box-shadow: ${p => p.theme.box.shadow};
+            padding: ${p => p.theme.padding.small};
+            height: 96px;
+            align-content: center;
+            white-space: nowrap;
+            gap: ${p => p.theme.gap.small};
+            width: 400px;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+
+            :hover {
+                color: ${p => p.theme.colors.primaryDark};
+
+                ::after {
+                    content: url("/images/icon/linkHover.svg");
+                }
+            }
+
+            div {
+                align-content: center;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                flex-grow: 1;
+            }
+
+            @media only screen and (max-width: ${p => p.theme.breakpoints.medium}) {
+                width: 250px;
+            }
         }
     }
 `
